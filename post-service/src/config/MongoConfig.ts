b07d38482922abb391003e309b254b0c39652890
Maksym Voloshko <anivone18052001@gmail.com>
2021-04-15 20:48:33 +0300

    [Post-Service/Data]: Mongo Models configured

---
+++ post-service/src/config/MongoConfig.ts
@@ -0,0 +1,10 @@
+import * as mongoose from "mongoose";
+import PostSchema, { IPostDocument, IPostModel } from "../data/schemas/PostSchema";
+
+export default function mongoModelsConfig(connection: mongoose.Connection) {
+   const postModel: IPostModel = connection.model<IPostDocument, IPostModel>('Post', PostSchema);
+
+   return {
+      postModel
+   }
+}
