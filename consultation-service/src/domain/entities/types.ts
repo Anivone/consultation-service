ffbbc140d97136b3d585039612a790b0a5827c28
Maksym Voloshko <anivone18052001@gmail.com>
2021-04-26 18:33:55 +0300

    [Consultation-Service/Application]: Added 'specialty' field to Consultation entity

--- consultation-service/src/domain/entities/types.ts
+++ consultation-service/src/domain/entities/types.ts
@@ -3,6 +3,7 @@ export interface IConsultation {
     userID: string;
     consultantID: string;
     sphereID: string;
+    specialty: string;
     companyName?: string;
     description: string;
     price: number;
